a7d74efd580745ab9b50bdcc1f3aeadbdbd5449b
nikhilr <nikhilr@macrotechit.com>
2025-11-14 22:27:09 +0530

    corrected path error '/'

--- ecom/client/monsta/src/apiServices/addressUpdate.js
+++ ecom/client/monsta/src/apiServices/addressUpdate.js
@@ -4,7 +4,7 @@ const API_BASE_URL = process.env.NEXT_PUBLIC_APIBASEURL;
 
 export const viewCompanyProfile = async () => {
     try {
-        const response = await axios.get(`${API_BASE_URL}user/view-company-profile`);
+        const response = await axios.get(`${API_BASE_URL}/user/view-company-profile`);
         return response.data;
     } catch (error) {
         console.error("Error Fetching Company Profile", error);
--- ecom/client/monsta/src/apiServices/testimonialView.js
+++ ecom/client/monsta/src/apiServices/testimonialView.js
@@ -4,7 +4,7 @@ const API_BASE_URL = process.env.NEXT_PUBLIC_APIBASEURL;
 
 export const viewTestimonials = async () => {
     try {
-        const response = await axios.get(`${API_BASE_URL}testimonial/view`);
+        const response = await axios.get(`${API_BASE_URL}/testimonial/view`);
         return response.data;
     } catch (error) {
         console.error("Error Fetching Testimonials", error);
