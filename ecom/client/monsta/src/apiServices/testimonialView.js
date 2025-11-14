d4b6747957b90f3211a6d4ce3c634208ecbfbc5b
nikhilr <nikhilr@macrotechit.com>
2025-11-14 22:28:18 +0530

    updated path with replace function

--- ecom/client/monsta/src/apiServices/testimonialView.js
+++ ecom/client/monsta/src/apiServices/testimonialView.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_APIBASEURL;
+const API_BASE_URL = process.env.NEXT_PUBLIC_APIBASEURL.replace(/\/+$/, '');
 
 export const viewTestimonials = async () => {
     try {
